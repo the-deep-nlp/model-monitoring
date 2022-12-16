81368798c9743de8f461b021c015d562a3e4fd42
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2022-12-16 15:34:33 +0545

    constant file added;

---
+++ constants.py
@@ -0,0 +1,26 @@
+SECTORS = ["Agriculture", "Cross", "Education", "Health", "Livelihoods", "Logistics", "Protection", "Shelter", "WASH", "Food Security", "Nutrition"]
+SUBPILLARS_1D = ["Context->Environment", "Context->Socio Cultural", "Context->Economy",
+    "Context->Demography", "Context->Legal & Policy", "Context->Security & Stability",
+    "Context->Politics", "Shock/Event->Type And Characteristics", "Shock/Event->Underlying/Aggravating Factors",
+    "Shock/Event->Hazard & Threats", "Displacement->Type/Numbers/Movements", "Displacement->Push Factors", "Displacement->Pull Factors",
+    "Displacement->Intentions", "Displacement->Local Integration", "Casualties->Injured", "Casualties->Missing", "Casualties->Dead",
+    "Humanitarian Access->Relief To Population", "Humanitarian Access->Population To Relief", "Humanitarian Access->Physical Constraints",
+    "Humanitarian Access->Number Of People Facing Humanitarian Access Constraints/Humanitarian Access Gaps",
+    "Information And Communication->Communication Means And Preferences", "Information And Communication->Information Challenges And Barriers",
+    "Information And Communication->Knowledge And Info Gaps (Pop)", "Information And Communication->Knowledge And Info Gaps (Hum)",
+    "Covid-19->Cases", "Covid-19->Contact Tracing", "Covid-19->Deaths", "Covid-19->Hospitalization & Care", "Covid-19->Restriction Measures",
+    "Covid-19->Testing", "Covid-19->Vaccination"]
+SUBPILLARS_2D = [
+    "At Risk->Number Of People At Risk", "At Risk->Risk And Vulnerabilities", "Capacities & Response->International Response", "Capacities & Response->Local Response",
+    "Capacities & Response->National Response", "Capacities & Response->Number Of People Reached/Response Gaps",
+    "Humanitarian Conditions->Coping Mechanisms", "Humanitarian Conditions->Living Standards",
+    "Humanitarian Conditions->Number Of People In Need", "Humanitarian Conditions->Physical And Mental Well Being",
+    "Impact->Driver/Aggravating Factors", "Impact->Impact On People", "Impact->Impact On Systems, Services And Networks",
+    "Impact->Number Of People Affected", "Priority Interventions->Expressed By Humanitarian Staff",
+    "Priority Interventions->Expressed By Population", "Priority Needs->Expressed By Humanitarian Staff", "Priority Needs->Expressed By Population"
+]
+CATEGORIES = [
+    "sectors",
+    "subpillars_1d",
+    "subpillars_2d",
+]
